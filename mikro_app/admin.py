3807971e0ed701ee9b73cc6e233ec5d9e2b3ee85
Mikita Vorobyov <olfb_c@ukr.net>
2014-04-16 23:37:12 +0300

    вирішено проблему з відображенням поля Language  для різної кількості записів в Tech_Info, лишається приєднати до шаблонів та система мови за замовчуванням

--- mikro_app/admin.py
+++ mikro_app/admin.py
@@ -6,44 +6,61 @@ from mikro_app.models import Tech_Info,Transport_Company,Orders,Static_Img,Conta
 def list_lang():
     list_name=list()
     all_lang=Language.objects.count()
-    if all_lang>0:
-        for l in xrange(1,all_lang+1):
-            lang_abbr=Language.objects.get(id=l).lang_abbr
-            list_name.append( (unicode(lang_abbr),unicode(lang_abbr)) ) 
-        return list_name
-    else:
-        list_name.append( (u'этот язык будет использован по умолчанию',
-                           u'этот язык будет использован по умолчанию') )
-        return list_name 
-
-class Tech_Info_Form_Choice(forms.ModelForm):	
-    lang=forms.ChoiceField(widget=forms.Select,choices=list_lang()) 
+    
+    for l in xrange(1,all_lang+1):
+        lang_abbr=Language.objects.get(id=l).lang_abbr
+        list_name.append((u'rus',u'rus'))
+        list_name.append((unicode(lang_abbr),unicode(lang_abbr)) ) 
+    return list_name
+    
+class Tech_Info_Form_Choice(forms.ModelForm):   
+    lang=forms.ChoiceField(label=u'Language',widget=forms.Select,choices=list_lang()) 
     class Meta:
         model = Tech_Info
-    
+'''  
 class Tech_Info_Form_Char_Default(forms.ModelForm):
     lang=forms.CharField(label=u'язык,установленный в системе по умолчанию')
     class Meta:
-        model = Tech_Info
+        model = Tech_Info'''
     
 class Tech_Info_Form_Char(forms.ModelForm):
     lang=forms.CharField(label=u'Language')
     class Meta:
         model = Tech_Info
-
+'''
 def one_or_more_languages():
     if Tech_Info.objects.count()>1:
-    	for all_tech_info in Tech_Info.objects.all():
-    		if all_tech_info.id>1:
-    			return Tech_Info_Form_Choice
-    		elif all_tech_info.id==1:
-    			return Tech_Info_Form_Char
+        for all_tech_info in Tech_Info.objects.all():
+            if all_tech_info.id>1:
+                return Tech_Info_Form_Choice
+            elif all_tech_info.id==1:
+                return Tech_Info_Form_Char
     else:
-    	return Tech_Info_Form_Char_Default
+        return Tech_Info_Form_Char_Default
 
+def readonly():
+    if one_or_more_languages()==Tech_Info_Form_Char_Default:
+        readonly_fields_list = ('lang',)
+    else:
+        readonly_fields_list = tuple()
+
+    return readonly_fields_list'''
+
+
+
+def one_or_more_languages():
+    #if Tech_Info.objects.count()==1:
+      #  return [Tech_Info_Form_Char_Default,('lang',)]
+
+    if Language.objects.count()>0:
+        return [Tech_Info_Form_Choice,list()]
+    else:
+        return [Tech_Info_Form_Char,list()]
+            
 
 class Tech_Info_Admin(admin.ModelAdmin):
-    form =one_or_more_languages()
+    form =one_or_more_languages()[0]
+   # readonly_fields=one_or_more_languages()[1]
 
 admin.site.register(Tech_Info,Tech_Info_Admin)
 admin.site.register(Transport_Company)
