70e77a9310cb727ff7b8f65bf478f949209f59ea
Mikita Vorobyov <olfb_c@ukr.net>
2014-05-08 19:56:32 +0300

    додано мультимовність для контактів- закінчено

--- mikro_app/forms.py
+++ mikro_app/forms.py
@@ -1,108 +1,111 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from mikro_app.models import Tech_Info,Transport_Company,Country,PaymentMethod
+from mikro_app.models import Tech_Info, Transport_Company, Country, PaymentMethod
 #from django.contrib.auth.models import User
 from django.forms.widgets import Input
- 
+
+
 class NumberInput(Input):
-#HTML5 Number Input."""
+# HTML5 Number Input."""
     input_type = 'number'
 
 
 def list_transport_company():
-    list_name=list()
-    all_comp=Transport_Company.objects.count()
-    for n in xrange(1,all_comp+1):
-        comp_name=Transport_Company.objects.get(id=n).name
-        list_name.append( (unicode(comp_name),unicode(comp_name)) ) #unicode(comp_name)
+    list_name = list()
+    all_comp = Transport_Company.objects.count()
+    for n in xrange(1, all_comp + 1):
+        comp_name = Transport_Company.objects.get(id=n).name
+        # unicode(comp_name)
+        list_name.append((unicode(comp_name), unicode(comp_name)))
     return list_name
 
+
 def list_num():
-    num=list()
-    for n in xrange(1,11):
-        n=unicode(n)
-        num.append((n,n))
-    return num     
+    num = list()
+    for n in xrange(1, 11):
+        n = unicode(n)
+        num.append((n, n))
+    return num
+
 
 def all_countries():
-    all_countries=list()
+    all_countries = list()
     for country_ins in Country.objects.all():
-        all_countries.append( (unicode(country_ins.country),unicode(country_ins.country)) )
+        all_countries.append(
+            (unicode(country_ins.country), unicode(country_ins.country)))
     return all_countries
 
 
 def list_payment_method():
-    all_payment_methods=list()
+    all_payment_methods = list()
     for payment_method_ins in PaymentMethod.objects.all():
-        all_payment_methods.append( (unicode(payment_method_ins.payment_method),
-                                     unicode(payment_method_ins.payment_method) )
-                                  )
+        all_payment_methods.append((unicode(payment_method_ins.payment_method),
+                                    unicode(payment_method_ins.payment_method))
+                                   )
     return all_payment_methods
 
 
-#    return [(u'наложенный платеж',u'наложенный платеж'),(u'банковская карта',u'банковская карта')]
+# return [(u'наложенный платеж',u'наложенный платеж'),(u'банковская
+# карта',u'банковская карта')]
 
-label_buy_num=Tech_Info.objects.get(id=1).label_buy_num
-label_fio=Tech_Info.objects.get(id=1).label_fio
-label_tel=Tech_Info.objects.get(id=1).label_tel
-label_city=Tech_Info.objects.get(id=1).label_city
-label_transport_company=Tech_Info.objects.get(id=1).label_transport_company
+label_buy_num = Tech_Info.objects.get(id=1).label_buy_num
+label_fio = Tech_Info.objects.get(id=1).label_fio
+label_tel = Tech_Info.objects.get(id=1).label_tel
+label_city = Tech_Info.objects.get(id=1).label_city
+label_transport_company = Tech_Info.objects.get(id=1).label_transport_company
 
-label_payment_method=Tech_Info.objects.get(id=1).label_payment_method
-info_payment_method=Tech_Info.objects.get(id=1).info_payment_method
+label_payment_method = Tech_Info.objects.get(id=1).label_payment_method
+info_payment_method = Tech_Info.objects.get(id=1).info_payment_method
 
-label_additional_information=Tech_Info.objects.get(id=1).label_additional_information
-info_additional_information=Tech_Info.objects.get(id=1).info_additional_information
+label_additional_information = Tech_Info.objects.get(
+    id=1).label_additional_information
+info_additional_information = Tech_Info.objects.get(
+    id=1).info_additional_information
 
-label_contacts_subject=Tech_Info.objects.get(id=1).label_contacts_subject
-label_contacts_name=Tech_Info.objects.get(id=1).label_contacts_name
-label_contacts_email=Tech_Info.objects.get(id=1).label_contacts_email
-label_contacts_text=Tech_Info.objects.get(id=1).label_contacts_text
-
-
-
+label_contacts_subject = Tech_Info.objects.get(id=1).label_contacts_subject
+label_contacts_name = Tech_Info.objects.get(id=1).label_contacts_name
+label_contacts_email = Tech_Info.objects.get(id=1).label_contacts_email
+label_contacts_text = Tech_Info.objects.get(id=1).label_contacts_text
 
 
 class Homepage_Form(forms.Form):
-    num=forms.ChoiceField(label=label_buy_num,widget=forms.Select, choices=list_num()) 
-    country=forms.ChoiceField(widget=forms.Select,choices=all_countries())
+    num = forms.ChoiceField(
+        label=label_buy_num, widget=forms.Select, choices=list_num())
+    country = forms.ChoiceField(widget=forms.Select, choices=all_countries())
+
 
 class Homepage_Form_Unique(forms.Form):
-    country=forms.ChoiceField(widget=forms.Select,choices=all_countries())
-
+    country = forms.ChoiceField(widget=forms.Select, choices=all_countries())
 
 
 class Orders_Form(forms.Form):
-    fio=forms.CharField(label=label_fio,max_length=100)
-    tel=forms.CharField(label=label_tel,max_length=20)
-    city=forms.CharField(label=label_city,max_length=100)
-    transport_company = forms.ChoiceField(widget=forms.Select,#используем выпадающий список 
-                                                 choices=list_transport_company(),
-                                                 label=label_transport_company,
-                                                 ) 
+    fio = forms.CharField(label=label_fio, max_length=100)
+    tel = forms.CharField(label=label_tel, max_length=20)
+    city = forms.CharField(label=label_city, max_length=100)
+    transport_company = forms.ChoiceField(widget=forms.Select,  # используем выпадающий список
+                                          choices=list_transport_company(
+                                          ),
+                                          label=label_transport_company,
+                                          )
 
-    payment_method=forms.ChoiceField(widget=forms.Select,#используем выпадающий список 
-                                                 choices=list_payment_method(),
-                                                 label=label_payment_method,
-                                                 help_text=info_payment_method) 
-    
-    additional_information=forms.CharField(label=label_additional_information,
-                                           max_length=10000,
-                                           help_text=info_additional_information,
-                                           widget=forms.Textarea(attrs={'cols': 60, 'rows':8}) )
+    payment_method = forms.ChoiceField(widget=forms.Select,  # используем выпадающий список
+                                       choices=list_payment_method(),
+                                       label=label_payment_method,
+                                       help_text=info_payment_method)
+
+    additional_information = forms.CharField(
+        label=label_additional_information,
+        max_length=10000,
+        help_text=info_additional_information,
+        widget=forms.Textarea(attrs={'cols': 60, 'rows': 8}))
 
 
-#class Confirmation(forms.Form):
+# class Confirmation(forms.Form):
 #   pass
 
 class Contacts (forms.Form):
-    contact_subject=forms.CharField(max_length=100)
-    name=forms.CharField(max_length=100)
-    email=forms.EmailField()
-    text=forms.CharField(max_length=10000,
-                         widget=forms.Textarea(attrs={'cols': 60, 'rows':10}) )
-
-
-     
-                                           
-                                          
+    contact_subject = forms.CharField(max_length=100)
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    text = forms.CharField(max_length=10000,
+                           widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
--- mikro_app/views.py
+++ mikro_app/views.py
@@ -1,186 +1,183 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
-from mikro_app.models import Tech_Info,Transport_Company,Orders,Contact,Static_Pages,Language,Currency,Country,PaymentMethod
-from mikro_app.forms import Homepage_Form,Homepage_Form_Unique,Orders_Form,Contacts
-from django.template import RequestContext 
+from mikro_app.models import Tech_Info, Transport_Company, Orders, Contact, Static_Pages, Language, Currency, Country, PaymentMethod
+from mikro_app.forms import Homepage_Form, Homepage_Form_Unique, Orders_Form, Contacts
+from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+
 def lang_id(lang):
-    if lang=='':
-        tech_info=Tech_Info.objects.get(id=1)
+    if lang == '':
+        tech_info = Tech_Info.objects.get(id=1)
     else:
-        tech_info=Tech_Info.objects.get(lang__lang_abbr=lang)
+        tech_info = Tech_Info.objects.get(lang__lang_abbr=lang)
     return tech_info
 
+
 def lang_def(lang):
-    if lang=='':
-        lang_abbr=Language.objects.get(default=True).lang_abbr
+    if lang == '':
+        lang_abbr = Language.objects.get(default=True).lang_abbr
     else:
-        lang_abbr=lang
+        lang_abbr = lang
     return lang_abbr
 
+
 def curr_def(curr):
-    if curr=='':
-        curr_abbr=Currency.objects.get(default=True).curr_abbr
+    if curr == '':
+        curr_abbr = Currency.objects.get(default=True).curr_abbr
     else:
-        curr_abbr=curr
+        curr_abbr = curr
     return curr_abbr
 
-def country_transport_company():
-    list_coutries=list()
-    list_t_c=list()
-    for country in Country.objects.all():
-        #list_t_c.append(country.country)
-        for set_all in country.transport_company_set.all():
-            list_t_c.append(set_all)
-        list_coutries.append(list_t_c)
-        list_t_c=list()
-    return list_coutries
 
+def start(request, lang='', curr=''):
+    tech_info = lang_id(lang)
+    lang = lang_def(lang)
+    curr = curr_def(curr)
 
-
-
-
-def start(request,lang='',curr=''): 
-    tech_info=lang_id(lang)
-    lang=lang_def(lang)
-    curr=curr_def(curr)
-
-    if tech_info.unique==True:
-        if request.method == 'POST':    
-            form=Homepage_Form_Unique(request.POST)
+    if tech_info.unique == True:
+        if request.method == 'POST':
+            form = Homepage_Form_Unique(request.POST)
             if form.is_valid():
                 fcd = form.cleaned_data
-                return HttpResponseRedirect (reverse('order_view', 
-                                             kwargs={'num':1,
-                                                     'lang':lang,
-                                                     'curr':curr,
-                                                     'country':fcd['country']}))
+                return HttpResponseRedirect(reverse('order_view',
+                                                    kwargs={'num': 1,
+                                                            'lang': lang,
+                                                            'curr': curr,
+                                                            'country': fcd['country']}))
         else:
-            form=Homepage_Form_Unique()
-        return render_to_response('start.html',{'form':form,
-                                                'lang':lang,
-                                                'tech_info':tech_info,
-                                                'curr':curr,
-                                                'view':'start'},
-                                          context_instance=RequestContext(request) )
+            form = Homepage_Form_Unique()
+        return render_to_response('start.html', {'form': form,
+                                                 'lang': lang,
+                                                 'tech_info': tech_info,
+                                                 'curr': curr,
+                                                 'view': 'start'},
+                                  context_instance=RequestContext(request))
 
     else:
-        if request.method == 'POST':    
-            form=Homepage_Form(request.POST)
+        if request.method == 'POST':
+            form = Homepage_Form(request.POST)
             if form.is_valid():
                 fcd = form.cleaned_data
-                return HttpResponseRedirect (reverse('order_view', 
-                                             kwargs={'num':fcd['num'],
-                                                     'lang':lang,
-                                                     'country':fcd['country'],
-                                                     'curr':curr}))
+                return HttpResponseRedirect(reverse('order_view',
+                                                    kwargs={'num': fcd['num'],
+                                                            'lang': lang,
+                                                            'country': fcd['country'],
+                                                            'curr': curr}))
         else:
-            form=Homepage_Form()
-        return render_to_response('start.html',{'form':form,
-                                                'lang':lang,
-                                                'tech_info':tech_info,
-                                                'curr':curr,
-                                                'view':'start'},
-                                          context_instance=RequestContext(request) )
+            form = Homepage_Form()
+        return render_to_response('start.html', {'form': form,
+                                                 'lang': lang,
+                                                 'tech_info': tech_info,
+                                                 'curr': curr,
+                                                 'view': 'start'},
+                                  context_instance=RequestContext(request))
 
-def order_view(request,num=1,lang='',curr='',country=''):
-    tech_info=lang_id(lang)
+
+def order_view(request, num=1, lang='', curr='', country=''):
+    tech_info = lang_id(lang)
 #    if num=='':
  #       num=1
- #   else:        
-    num=int(num)
-    if request.method == 'POST':    
-        form=Orders_Form(request.POST)
+ #   else:
+    num = int(num)
+    if request.method == 'POST':
+        form = Orders_Form(request.POST)
         if form.is_valid():
             fcd = form.cleaned_data
 
-            t_c=Transport_Company.objects.get(name=fcd['transport_company'])
-            price=Currency.objects.get(curr_abbr=curr).curr_price                               
-            s=num*price        
-            new_order= Orders (
-                          num=num,
-                          sum_price=s,
-                          curr=curr,
-                          fio=fcd['fio'],
-                          tel=fcd['tel'],
-                          country=Country.objects.get(country=country),
-                          city=fcd['city'],
-                          transport_company=fcd['transport_company'],
-                          payment_method=PaymentMethod.objects.get(payment_method=fcd['payment_method']),
-                          additional_information=fcd['additional_information']
-                        )
+            t_c = Transport_Company.objects.get(name=fcd['transport_company'])
+            price = Currency.objects.get(curr_abbr=curr).curr_price
+            s = num * price
+            new_order = Orders(
+                num=num,
+                sum_price=s,
+                curr=curr,
+                fio=fcd['fio'],
+                tel=fcd['tel'],
+                country=Country.objects.get(country=country),
+                city=fcd['city'],
+                transport_company=fcd['transport_company'],
+                payment_method=PaymentMethod.objects.get(
+                    payment_method=fcd['payment_method']),
+                additional_information=fcd['additional_information']
+            )
             new_order.save()
-            if Tech_Info.objects.get(id=1).unique==True:
-                unique=True
+            if Tech_Info.objects.get(id=1).unique == True:
+                unique = True
             else:
-                unique=False
-            form_homepage=Homepage_Form()
-            return render_to_response('start.html',{'thanks_for_buying':tech_info.thanks_for_buying,
-                                                    'tech_info':tech_info,
-                                                    'num':num,
-                                                    'lang':lang,
-                                                    'curr':curr,
-                                                    'view':'start',
-                                                    'sum_price':s,'form':form_homepage}, 
-                                       context_instance=RequestContext(request) )               
+                unique = False
+            form_homepage = Homepage_Form()
+            return render_to_response(
+                'start.html', {'thanks_for_buying': tech_info.thanks_for_buying,
+                               'tech_info': tech_info,
+                               'num': num,
+                               'lang': lang,
+                               'curr': curr,
+                               'view': 'start',
+                               'sum_price': s, 'form': form_homepage},
+                context_instance=RequestContext(request))
     else:
-        form=Orders_Form()      
-    return render_to_response('order.html',{'form':form,
-                                            'tech_info':tech_info,
-                                             'lang':lang,
-                                             'curr':curr,
-                                             'num':num,
-                                             'transport_company_filter_country':Transport_Company.objects.filter(country__country=country),
-                                             'transport_company_all':Transport_Company.objects.all(),
-                                             'country':country,
-                                             'view':'order_view'}, 
-                                       context_instance=RequestContext(request) )
+        form = Orders_Form()
+    return render_to_response('order.html', {'form': form,
+                                             'tech_info': tech_info,
+                                             'lang': lang,
+                                             'curr': curr,
+                                             'num': num,
+                                             'transport_company_filter_country': Transport_Company.objects.filter(country__country=country),
+                                             'transport_company_all': Transport_Company.objects.all(),
+                                             'country': country,
+                                             'view': 'order_view'},
+                              context_instance=RequestContext(request))
 
-def shipping_and_payment(request,lang='',curr=''):
-    tech_info=lang_id(lang)
+
+def shipping_and_payment(request, lang='', curr=''):
+    tech_info = lang_id(lang)
     return render_to_response('shipping_and_payment.html',
-                             {'tech_info':tech_info,
-                             'lang':lang,
-                             'curr':curr,
-                             'view':'shipping_and_payment'}, 
-                              context_instance=RequestContext(request) )
+                              {'tech_info': tech_info,
+                               'lang': lang,
+                               'curr': curr,
+                               'view': 'shipping_and_payment'},
+                              context_instance=RequestContext(request))
 
-def contacts(request,lang='',curr=''):
-    tech_info=lang_id(lang)
+
+def contacts(request, lang='', curr=''):
+    tech_info = lang_id(lang)
     all_is_right = ''
 
-    if request.method == 'POST':    
-        form=Contacts(request.POST)
+    if request.method == 'POST':
+        form = Contacts(request.POST)
         if form.is_valid():
             fcd = form.cleaned_data
-            new_letter=Contact(contact_subject=fcd['contact_subject'],
-                               name=fcd['name'] ,
-                               email=fcd['email'] ,
-                               text=fcd['text']
-                               )
+            new_letter = Contact(contact_subject=fcd['contact_subject'],
+                                 name=fcd['name'],
+                                 email=fcd['email'],
+                                 text=fcd['text']
+                                 )
             new_letter.save()
-            all_is_right = Tech_Info.objects.get(id=1).contacts_thanks            
-            form=Contacts()
+            all_is_right = Tech_Info.objects.get(id=1).contacts_thanks
+            form = Contacts()
            # form_homepage=Homepage_Form()
            # return render_to_response('start.html',{'contacts_thanks':Tech_Info.objects.get(id=1).contacts_thanks,
-            #                                        'form':form_homepage}, 
-            #                           context_instance=RequestContext(request) )
+            #                                        'form':form_homepage},
+            # context_instance=RequestContext(request) )
     else:
-        form=Contacts()
-    return render_to_response('contacts.html',{'form':form,
-                                               'all_is_right': all_is_right,
-                                               'tech_info':tech_info,
-                                               'lang':lang,
-                                               'curr':curr,
-                                               'view':'contacts'}, 
-                                       context_instance=RequestContext(request) )
-            
-def static_page(request,num,lang='',curr=''):
-    tech_info=lang_id(lang)                                  
-    return render_to_response('static_page.html',{'static_page':Static_Pages.objects.get(num=num),
-                                                  'tech_info':tech_info,
-                                                  'lang':lang,
-                                                  'curr':curr, 
-                                                  'view':'static_page'},
-                                                   context_instance=RequestContext(request))
+        form = Contacts()
+    return render_to_response('contacts.html', {'form': form,
+                                                'all_is_right': all_is_right,
+                                                'tech_info': tech_info,
+                                                'lang': lang,
+                                                'curr': curr,
+                                                'view': 'contacts'},
+                              context_instance=RequestContext(request))
+
+
+def static_page(request, num, lang='', curr=''):
+    tech_info = lang_id(lang)
+    return render_to_response(
+        'static_page.html', {'static_page': Static_Pages.objects.get(num=num),
+                             'tech_info': tech_info,
+                             'lang': lang,
+                             'curr': curr,
+                             'view': 'static_page'},
+        context_instance=RequestContext(request))
